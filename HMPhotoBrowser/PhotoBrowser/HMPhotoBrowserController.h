5949fa4048244423cc9de438ff73263765978438
刀哥 <liufan321@gmail.com>
2016-03-07 00:59:38 +0800

    add empty photo browser controller

---
+++ HMPhotoBrowser/PhotoBrowser/HMPhotoBrowserController.h
@@ -0,0 +1,14 @@
+//
+//  HMPhotoBrowserController.h
+//  HMPhotoBrowser
+//
+//  Created by 刘凡 on 16/3/7.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/// 图片浏览控制器
+@interface HMPhotoBrowserController : UIViewController
+
+@end
